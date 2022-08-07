019c4b00f7821d8eff268854b2f17d8e42fb6d8e
Vitali Ahel <nonsens_bill@mail.ru>
2022-08-07 22:26:36 +0300

    docs: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    const number = Math.abs(n);
+    const arrStr = number.toString().split('').reverse();
+    let str = '';
+    for (let i = 0; i < arrStr.length; i++) {
+        str = str + arrStr[i];
+
+    }
+  return str
 }
